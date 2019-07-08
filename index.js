cc009534c2c64c3a0b814f9f7c5e9a9d5778bf27
Max <maxiroitnerqgmail.com>
2019-07-08 19:02:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function codeAddress() {
-            alert('At the moment we are still working on Chlamydika the RPG therefore we would be glad if you could support us on Kickstarter. Even the smallest amaout ist still a enormously help for us to develop this game!                                                                                                                                           Thank you for your understanding');
+            alert('At the moment we are still working on Chlamydika the RPG therefore we would be glad if you could support us on Kickstarter. Even the smallest amaout ist still a enormous help for us to develop this game!                                                                                                                                           Thank you for your understanding');
         }
         window.onload = codeAddress;
