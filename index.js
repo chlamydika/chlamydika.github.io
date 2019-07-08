18801851a76143e6fad97a271c2831dd0f6e1f22
Max <maxiroitnerqgmail.com>
2019-07-08 18:06:48 +0200

    Alert kicksterter

---
+++ index.js
@@ -0,0 +1,5 @@
+
+function codeAddress() {
+            alert('At the moment we are still working on Chlamydika the RPG therefore we would be glad if you could support us on Kickstarter. Even the smallest amaout ist still a enormously help for us to develop this game!                                                                                                                                           Thank you for your understanding');
+        }
+        window.onload = codeAddress;
